7f9bacd69c90ddcf0468ad28ef96a54cbbb040e0
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-04-14 20:28:57 +0200

    7.2 - Task: create immutable class - Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -15,5 +16,8 @@ public class StreamMain {
         System.out.println(poemBeautifier.beautify("I love Lambda Expressions", decorator2));
         System.out.println(poemBeautifier.beautify("I love Lambda Expressions", decorator3));
         System.out.println(poemBeautifier.beautify("I love Lambda Expressions", decorator4));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
