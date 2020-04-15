59aa65c0cc690b096eebc1f044bd15859927a14e
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-04-15 14:28:27 +0200

    Removed unnececcery imports from StreamMain class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,16 +1,9 @@
 package com.kodilla.stream;
 
-
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
-import com.kodilla.stream.person.People;
-
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class StreamMain {
