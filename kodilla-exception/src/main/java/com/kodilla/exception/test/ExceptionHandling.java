e4fddf3591d3167b031b9d49f8a7131b84702ad0
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-04-22 13:56:30 +0200

    8.3 Task: Throw, Throws. SecondChallenge

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args){
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try{
+            secondChallenge.probablyIWillThrowException(2,0);
+        } catch (Exception e){
+            System.out.println("Error: x >= 2 or x < 1 or y = 1.5!");
+        }
+    }
+}
