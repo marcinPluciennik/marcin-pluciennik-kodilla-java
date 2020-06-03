c2bb3e84892d651ac1535e498289f8582e9fd7a8
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-06-03 17:33:17 +0200

    Task 14.2

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,4 +1,27 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculations(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double resultAdd = calculator.add(1,1);
+        double resultSub = calculator.sub(1,1);
+        double resultMul = calculator.mul(1,1);
+        double resultDiv = calculator.div(1,1);
+        //Than
+        //do nothing
+    }
 }
