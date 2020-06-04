8cdb7ededd880c6943ae3a9066c4025c859d0731
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-06-04 16:27:36 +0200

    Task 14.3

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -1,4 +1,21 @@
 package com.kodilla.spring.portfolio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskList {
+    List<String> tasks;
+
+    public TaskList(){
+        tasks = new ArrayList<>();
+    }
+
+    public String getTask() {
+        String task = tasks.get(0);
+        return task;
+    }
+
+    public void addTaskToList(String task){
+        tasks.add(task);
+    }
 }
