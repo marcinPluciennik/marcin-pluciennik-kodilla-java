3a4036c41837450ba41d87c7691da5e83ad24c91
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-06-04 16:26:40 +0200

    Spring Examples

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,4 +1,28 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BoardConfig {
+
+    @Bean
+    public Board getBoard() {
+        return new Board(toDoTaskList(), inProgressTaskList(), doneTaskList());
+    }
+
+    @Bean
+    public TaskList toDoTaskList() {
+        return new TaskList();
+    }
+
+    @Bean
+    public TaskList inProgressTaskList() {
+        return new TaskList();
+    }
+
+    @Bean
+    public TaskList doneTaskList() {
+        return new TaskList();
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,4 +1,26 @@
 package com.kodilla.spring.portfolio;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class BoardTestSuite {
+
+    @Test
+    public void testTaskAdd(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        //When
+        Board board = context.getBean(Board.class);
+
+        board.getToDoList().addTaskToList("Watch TV");
+        board.getInProgressList().addTaskToList("Read the book");
+        board.getDoneList().addTaskToList("Buy new T-shirt");
+
+        //Then
+        Assert.assertEquals("Watch TV", board.getToDoList().getTask());
+        Assert.assertEquals("Read the book", board.getInProgressList().getTask());
+        Assert.assertEquals("Buy new T-shirt", board.getDoneList().getTask());
+    }
 }
