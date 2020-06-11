ed9bda2363d4cc6b6bfe6c08904874920b00bd95
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-06-11 20:03:29 +0200

    Task 15.5

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+
+    @Test
+    public void testBigmacNew(){
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("Bacon")
+                .ingredient("Onion")
+                .bun("Sesame bun")
+                .sauce("Standard")
+                .ingredient("Cheese")
+                .burgers(3)
+                .ingredient("Chilli")
+                .build();
+
+        System.out.println(bigmac);
+
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+
+        //Than
+        Assert.assertEquals(4, howManyIngredients);
+    }
+}
