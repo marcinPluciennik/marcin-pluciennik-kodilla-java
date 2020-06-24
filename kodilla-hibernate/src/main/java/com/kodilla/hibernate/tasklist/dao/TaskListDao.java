66d28d0734ce4e45aab3e8c91a00d33537266657
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-06-24 12:08:27 +0200

    Fix CrudRepository<TaskList, Integer>

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
@@ -4,6 +4,6 @@ import com.kodilla.hibernate.tasklist.TaskList;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
-public interface TaskListDao extends CrudRepository<TaskList, String> {
+public interface TaskListDao extends CrudRepository<TaskList, Integer> {
     List<TaskList> findByListName(String listName);
 }
