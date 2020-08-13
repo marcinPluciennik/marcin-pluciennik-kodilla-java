98180a1ac1e59df4d40854e6e8e2f8da870d5314
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-08-13 16:52:19 +0200

    Example 24.5

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/company/oldhrsystem/Workers.java
@@ -0,0 +1,34 @@
+package com.kodilla.patterns2.adapter.company.oldhrsystem;
+
+public class Workers {
+    private String[][] workers = {
+            {"11111111111", "John", "Smith"},
+            {"22222222222", "Ivone", "Novak"},
+            {"33333333333", "Walter", "White"},
+            {"44444444444", "Clara", "Lanson"},
+            {"55555555555", "Jessie", "Pinkman"}
+    };
+
+    private double[] salaries = {
+            4500.00,
+            3700.00,
+            4350.00,
+            9000.00,
+            6200.00
+    };
+
+    public String getWorker(int n){
+        if (n > salaries.length){
+            return "";
+        }
+        return workers [n][0] + ", " + workers [n][1] + ", " + workers [n][2] + ", " + salaries[n];
+    }
+
+    public String[][] getWorkers() {
+        return workers;
+    }
+
+    public double[] getSalaries() {
+        return salaries;
+    }
+}
---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/company/SalaryAdapterTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns2.adapter.company;
+
+import com.kodilla.patterns2.adapter.company.oldhrsystem.Workers;
+import org.junit.Test;
+
+import java.awt.desktop.SystemEventListener;
+
+import static org.junit.Assert.assertEquals;
+
+public class SalaryAdapterTestSuite {
+
+    @Test
+    public void testTotalSalary(){
+        //Given
+        Workers workers = new Workers();
+        SalaryAdapter salaryAdapter = new SalaryAdapter();
+
+        //When
+        double totalSalary = salaryAdapter.TotalSalary(workers.getWorkers(), workers.getSalaries());
+
+        //Than
+        System.out.println(totalSalary);
+        assertEquals(totalSalary,27750, 0);
+    }
+}
