5d21a95582f813696e1a539b4a17e6c3b7b37a37
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-10-21 21:34:36 +0200

    Task 30.3 Zadanie: Wywołanie procedury UpdateBestsellers z poziomu Javy

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -31,4 +31,26 @@ public class StoredProcTestSuite {
         }
         Assert.assertEquals(0, howMany);
     }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException{
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLERS=null";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBestseller()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLERS=null";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()){
+            howMany = rs.getInt("HOW_MANY");
+        }
+        Assert.assertEquals(0, howMany);
+    }
 }
